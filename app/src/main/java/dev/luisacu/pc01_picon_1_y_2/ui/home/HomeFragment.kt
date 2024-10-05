25e2f17fbda94b6038646d86746accbf5595ca28
Adan28742 <20101034@ue.edu.pe>
2024-10-05 10:31:54 -0500

    Avance_3

--- app/src/main/java/dev/luisacu/pc01_picon_1_y_2/ui/home/HomeFragment.kt
+++ app/src/main/java/dev/luisacu/pc01_picon_1_y_2/ui/home/HomeFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -12,9 +14,8 @@ import dev.luisacu.pc01_picon_1_y_2.databinding.FragmentHomeBinding
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
+    private lateinit var listViewTeams: ListView
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -32,6 +33,23 @@ class HomeFragment : Fragment() {
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+        listViewTeams = binding.listViewTeams
+        val teams = arrayOf(
+            "FC Barcelona",
+            "Real Madrid",
+            "Manchester United",
+            "Bayern Munich",
+            "Paris Saint-Germain",
+            "Juventus",
+            "Liverpool",
+            "Chelsea",
+            "Manchester City",
+            "Arsenal"
+        )
+
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, teams)
+        listViewTeams.adapter = adapter
+
         return root
     }
 
